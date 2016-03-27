196e4325bb44d746cf501324244af491e19a7725
Matthias Mailänder <matthias@mailaender.name>
2016-03-27 19:03:16 +0200

    Add Yuri with mind control.

---
+++ OpenRA.Mods.RA2/Traits/Attack/AttackLoyalty.cs
@@ -0,0 +1,93 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Activities;
+using OpenRA.Traits;
+using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.RA2.Traits
+{
+	[Desc("Can change owners of victims with `MindControllable` trait.")]
+	public class AttackLoyaltyInfo : AttackBaseInfo
+	{
+		public readonly int TargetsAtOnce = 1;
+
+		public override object Create(ActorInitializer init) { return new AttackLoyalty(init.Self, this); }
+	}
+
+	public class AttackLoyalty : AttackBase, INotifyKilled
+	{
+		readonly AttackLoyaltyInfo info;
+
+		public AttackLoyalty(Actor self, AttackLoyaltyInfo info)
+			: base(self, info)
+		{
+			this.info = info;
+		}
+
+		protected override bool CanAttack(Actor self, Target target)
+		{
+			if (target.Actor.TraitOrDefault<MindControllable>() == null)
+				return false;
+
+			return base.CanAttack(self, target);
+		}
+
+		public override Activity GetAttackActivity(Actor self, Target newTarget, bool allowMove, bool forceAttack)
+		{
+			return new MindControl(this, newTarget, info);
+		}
+
+		public List<Actor> Victims = new List<Actor>();
+
+		class MindControl : Activity
+		{
+			readonly Target target;
+			readonly AttackLoyalty attack;
+			readonly AttackLoyaltyInfo info;
+
+			public MindControl(AttackLoyalty attack, Target target, AttackLoyaltyInfo info)
+			{
+				this.target = target;
+				this.attack = attack;
+				this.info = info;
+			}
+
+			public override Activity Tick(Actor self)
+			{
+				if (IsCanceled || !target.IsValidFor(self) || !attack.CanAttack(self, target) || attack.Victims.Count > info.TargetsAtOnce)
+					return NextActivity;
+
+				var mindControllable = target.Actor.TraitOrDefault<MindControllable>();
+				if (mindControllable != null)
+				{
+					attack.DoAttack(self, target);
+					mindControllable.ChangeOwner(target.Actor, self);
+					attack.Victims.Add(target.Actor);
+				}
+
+				return this;
+			}
+		}
+
+		public void Killed(Actor self, AttackInfo e)
+		{
+			foreach (var victim in Victims)
+			{
+					var mindControllable = victim.TraitOrDefault<MindControllable>();
+					if (mindControllable != null)
+						mindControllable.RevertOwner(victim);
+			}
+		}
+	}
+}
---
+++ OpenRA.Mods.RA2/Traits/MindControllable.cs
@@ -0,0 +1,63 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA2.Traits
+{
+	[Desc("Interacts with the `AttackLoyality` trait.")]
+	public class MindControllableInfo : ITraitInfo
+	{
+		public object Create(ActorInitializer init) { return new MindControllable(init.Self, this); }
+	}
+
+	public class MindControllable : INotifyOwnerChanged, INotifyKilled
+	{
+		Player originalOwner;
+		Player changingOwner;
+
+		Actor controller;
+
+		public MindControllable(Actor self, MindControllableInfo info)
+		{
+			originalOwner = self.Owner;
+		}
+
+		public void ChangeOwner(Actor self, Actor attacker)
+		{
+			controller = attacker;
+			changingOwner = attacker.Owner;
+			self.ChangeOwner(attacker.Owner);
+			self.CancelActivity(); // Stop shooting, you have got new enemies
+		}
+
+		public void RevertOwner(Actor self)
+		{
+			changingOwner = originalOwner;
+			self.ChangeOwner(originalOwner);
+		}
+
+		public void OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+		{
+			if (changingOwner == null || changingOwner != newOwner)
+				originalOwner = newOwner; // It wasn't a temporary change, so we need to update here
+			else
+				changingOwner = null; // It was triggered by this trait: reset
+		}
+
+		public void Killed(Actor self, AttackInfo e)
+		{
+			var mindController = controller.TraitOrDefault<AttackLoyalty>();
+			if (mindController != null)
+				mindController.Victims.Remove(self);
+		}
+	}
+}
